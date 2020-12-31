5c4aa8e6f42e4ad949d2b4eb716d82f4a8972e74
JangYongUn <sm970413@naver.com>
2020-12-31 16:07:41 +0900

    ...

---
+++ js/index.js
@@ -0,0 +1,21 @@
+/********* 전역선언 **********/
+
+
+
+/********* 사용자함수 **********/
+
+
+/********* 이벤트선언 **********/
+var swiper = new Swiper('.swiper-container', {
+	slidesPerView: 1,
+	loop: true,
+	showSlides: 2000,
+	pagination: {
+	  el: '.swiper-pagination',
+	  clickable: true,
+	},
+  });
+
+
+/********* 이벤트콜백 **********/
+
